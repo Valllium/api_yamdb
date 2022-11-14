3c68b6c14d12a2362a680d0ecd352d729965bc2a
Александр Дюжев <frduke@mail.ru>
2022-11-14 16:06:23 +0300

    Добавлена рабочая модель пользователя.

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -3,14 +3,20 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
-class UserRole(models.TextChoices):
-    USER = "user"
-    MODERATOR = "moderator"
-    ADMIN = "admin"
+# class UserRole(models.TextChoices):
+#    USER = "user"
+#    MODERATOR = "moderator"
+#    ADMIN = "admin"
 
 
 class User(AbstractUser):
     """Расширенная модель пользователя."""
+    USER_ROLES = (
+        ('USER', 'user'),
+        ('MODERATOR', "moderator"),
+        ('ADMIN', "admin"),
+    )
     email = models.EmailField(_('Электронная почта'), max_length=254)
     bio = models.TextField(_("Биография"), max_length=256, blank=True)
-    role = models.CharField(_("Права доступа"), max_length=150, choices=UserRole.choices, default=UserRole.USER)
+    role = models.CharField(max_length=10, choices=USER_ROLES, default='USER')
+    # role = models.CharField(max_length=10, choices=UserRole.choices, default=UserRole.USER)
