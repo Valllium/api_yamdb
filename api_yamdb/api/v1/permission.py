56bd74b89209f2cd3933ae508b96ac80443d98f4
Александр Дюжев <frduke@mail.ru>
2022-11-22 13:57:14 +0300

    registration complete

--- api_yamdb/api/v1/permission.py
+++ api_yamdb/api/v1/permission.py
@@ -46,7 +46,6 @@ class UserAndModeratorOrReadOnly(BasePermission):
 
 class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
-        return (
-            request.method in SAFE_METHODS
-            or (request.user.is_authenticated and request.user.is_admin)
+        return request.method in SAFE_METHODS or (
+            request.user.is_authenticated and request.user.is_admin
         )
--- api_yamdb/api/v1/views.py
+++ api_yamdb/api/v1/views.py
@@ -9,7 +9,11 @@ from rest_framework import mixins, status
 from rest_framework.decorators import action
 from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.pagination import LimitOffsetPagination
-from rest_framework.permissions import AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import (
+    AllowAny,
+    IsAuthenticated,
+    IsAuthenticatedOrReadOnly,
+)
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet, ModelViewSet, ViewSet
@@ -17,7 +21,12 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from reviews.models import Category, Genre, Review, Title
 from users.models import User
 
-from .permission import IsAdministrator, IsAuthorOrIsStaffPermission, ReadOnly, IsAdminOrReadOnly
+from .permission import (
+    IsAdministrator,
+    IsAdminOrReadOnly,
+    IsAuthorOrIsStaffPermission,
+    ReadOnly,
+)
 from .serializers import (
     CategorySerializer,
     CommentSerializer,
@@ -104,21 +113,19 @@ class GetTokenAPIView(APIView):
 
     def post(self, request):
         serializer = UserTokenReceivingSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            user = get_object_or_404(
-                User, username=serializer.validated_data.get("username")
-            )
-            if default_token_generator.check_token(
-                user, serializer.validated_data.get("confirmation_code")
-            ):
-                token = RefreshToken.for_user(user)
-                return Response(
-                    {"token": f"{token}"}, status=status.HTTP_200_OK
-                )
-            return Response(
-                {"confirmation code": "Некорректный код подтверждения!"},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
+        serializer.is_valid(raise_exception=True)
+        user = get_object_or_404(
+            User, username=serializer.validated_data.get("username")
+        )
+        if default_token_generator.check_token(
+            user, serializer.validated_data.get("confirmation_code")
+        ):
+            token = RefreshToken.for_user(user)
+            return Response({"token": f"{token}"}, status=status.HTTP_200_OK)
+        return Response(
+            {"confirmation code": "Некорректный код подтверждения!"},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
 
 
 class ReviewViewSet(ModelViewSet):
@@ -194,58 +201,60 @@ class CategoryViewSet(ListCreateDeleteViewSet):
     permission_classes = [IsAuthenticated & IsAdministrator | ReadOnly]
 
 
-#class ListCreateDeleteViewSet(
+# class ListCreateDeleteViewSet(
 #    mixins.ListModelMixin,
 #    mixins.CreateModelMixin,
 #    mixins.DestroyModelMixin,
 #    GenericViewSet,
-#):
+# ):
 #    pass
 
 
-#class TitleViewSet(ViewSet):
+# class TitleViewSet(ViewSet):
 #    """ViewSet для эндпойнта /Title/
 #    c пагинацией и фильтрацией  по всем полям"""
 
-    # queryset = Title.objects.all()
-    # serializer_class = TitleSerializer
-    # pagination_class = LimitOffsetPagination
-    #    filter_backends = [DjangoFilterBackend, OrderingFilter]
-    #    permission_classes = [IsAdminUser]
-    # filter_backends = (SearchFilter,)
-    # filterset_fields = (
-    #     "name",
-    #   "year",
-    #    "genre__slug",
-    #     "category__slug",
-    # )
-    # ordering_fields = (
-    #     "name",
-    #     "year",
-    # )
+# queryset = Title.objects.all()
+# serializer_class = TitleSerializer
+# pagination_class = LimitOffsetPagination
+#    filter_backends = [DjangoFilterBackend, OrderingFilter]
+#    permission_classes = [IsAdminUser]
+# filter_backends = (SearchFilter,)
+# filterset_fields = (
+#     "name",
+#   "year",
+#    "genre__slug",
+#     "category__slug",
+# )
+# ordering_fields = (
+#     "name",
+#     "year",
+# )
 
-    #def list(self, request):
-    #    queryset = Title.objects.all()
-    #    serializer = TitleSerializer(queryset, many=True)
-    #    return Response(serializer.data)
+# def list(self, request):
+#    queryset = Title.objects.all()
+#    serializer = TitleSerializer(queryset, many=True)
+#    return Response(serializer.data)
 
-    #def retrieve(self, request, id=None):
-    #    queryset = Title.objects.all()
-    #    title = get_object_or_404(queryset, id=pk)
-    #    serializer = TitleSerializer(title)
-    #    return Response(serializer.data)
+# def retrieve(self, request, id=None):
+#    queryset = Title.objects.all()
+#    title = get_object_or_404(queryset, id=pk)
+#    serializer = TitleSerializer(title)
+#    return Response(serializer.data)
 
-    #def create(self, request):
-    #    serializer = TitleSerializerCreate(data=request.data)
-    #    if serializer.is_valid():
-    #        serializer.save()
-    #        return Response(serializer.data, status=status.HTTP_200_OK)
-    #    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# def create(self, request):
+#    serializer = TitleSerializerCreate(data=request.data)
+#    if serializer.is_valid():
+#        serializer.save()
+#        return Response(serializer.data, status=status.HTTP_200_OK)
+#    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    # return Response(serializer.data, status=status.HTTP_201_CREATED)
+# return Response(serializer.data, status=status.HTTP_201_CREATED)
+
 
 class TitleViewSet(ModelViewSet):
     """Отображение действий с произведениями"""
+
     permission_classes = [IsAdminOrReadOnly]
     queryset = Title.objects.all()
     filter_backends = (SearchFilter,)
@@ -257,6 +266,6 @@ class TitleViewSet(ModelViewSet):
     )
 
     def get_serializer_class(self):
-        if self.action in ('list', 'retrieve'):
+        if self.action in ("list", "retrieve"):
             return TitleSerializer
         return TitleSerializerCreate
